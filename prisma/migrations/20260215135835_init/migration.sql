41105f331ff0ea4eb1afc22c159e31b294569d16
sombit-k <sombitkarmakar018@gmail.com>
2026-02-15 19:35:45 +0530

    chore: add initial migration files for Project table and migration lock

---
+++ prisma/migrations/20260215135835_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "Project" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "githubUrl" TEXT,
+    "liveUrl" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Project_slug_key" ON "Project"("slug");
